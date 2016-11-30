8e72b25383f6bbe1e75ba4e6fd9b580481f9220d
Danillo Souza <danillo012@gmail.com>
2016-11-30 14:59:36 -0200

    Starting module to log tasks executions

---
+++ lib/Alfred/Logger.pm
@@ -0,0 +1,15 @@
+package Alfred::Logger;
+require Exporter;
+
+our @ISA       = qw/Exporter/;
+our @EXPORT    = qw//;
+our @EXPORT_OK = qw//;
+our $VERSION   = 1.00;
+
+
+#
+# TODO: Module to log the result of task executions.
+#
+
+
+42;
