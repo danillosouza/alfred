3e1c3c004c964368495bdda43881110e1c8bc05e
Danillo Souza <danillo012@gmail.com>
2016-12-01 12:14:03 -0200

    Template of POD documentation for created modules and updating dummy module

--- tasks/Foo/Bar.pm
+++ tasks/Foo/Bar.pm
@@ -16,13 +16,35 @@ our @EXPORT_OK = qw//;
 our $VERSION   = 1.00;
 
 
+##
+## Entry Point
 sub main {
     say('Heeeeeey!');
 }
 
+
+##
+## Help text for your task
 sub help {
     say('Hooooooo!');
 }
 
-
 42;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+${task} - ** Short description of yout Task **
+
+=head1 SYNOPSIS
+
+** What your task does **
+
+=head1 DESCRIPTION
+
+** Full description and any other info you want to include **
+
+=cut
